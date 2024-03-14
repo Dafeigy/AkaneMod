d63de2c42e0b104223a10104f33246e9e37e10f9
Dafeigy <1070642765@qq.com>
2024-03-14 15:13:57 +0800

    <Update>:Init Akane Repo with BepInex

---
+++ AkaneMod/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using UnityEngine;
+
+namespace AkaneMod
+{
+    [BepInPlugin("cn.cybershit.plugin.AkaneMod", "AkaneMod","1.0.0")]
+    public class AkaneMod : BaseUnityPlugin
+    {
+        void Start()
+        {
+            Logger.LogInfo("Hello wolrd from Logger."); // Better use this to print log.
+        }
+        
+    }
+}
